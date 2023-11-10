6e03d38773c48f43df28904f7aa2e1294ec7d1b3
Farazjel <patryk.j.redmer@gmail.com>
2023-11-10 19:48:18 +0100

    zad

--- cypress/e2e/RandomParameters.cy.js
+++
@@ -1,7 +0,0 @@
-it('Checking Random Parameters', () => {
-    const randomParam = Math.random().toString(36).substring(7);
-    cy.request(`GET', 'https://httpbin.org/get?randomParam=${randomParam}`).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.args.randomParam).to.eq(randomParam);
-    });
-  });
--- cypress/e2e/Redirection.cy.js
+++
@@ -1,6 +0,0 @@
-it('Redirect', () => {
-    cy.request('GET', 'https://httpbin.org/redirect-to?url=https://httpbin.org/get').then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.redirectedToUrl).to.include('https://httpbin.org/get');
-    });
-  });
